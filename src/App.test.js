241607660d3d12268779caa01e3a8f4c197b2763
SergeyKhrapin <khrapins@gmail.com>
2021-02-25 13:59:13 +0200

    Unit tests

--- src/App.test.js
+++ src/App.test.js
@@ -9,33 +9,8 @@ import App from './App';
 import * as helpers from './helpers';
 import * as actionCreators from './redux/actionCreators';
 import * as actionTypes from './redux/actionTypes';
-import { cartReducer } from './redux/reducers/cartReducer';
+import * as reducer from './redux/reducers/cartReducer';
 import * as constant from './constants';
-
-const stateMock = {
-    products: {
-        1000001: {
-            product: { id: 1000001, price: 50, title: "Heme" },
-            quantity: 2
-        },
-        1000003: {
-            product: { id: 1000003, price: 40, title: 'MASHIKO' },
-            quantity: 3
-        }
-    },
-    totalQuantity: 5,
-    totalPrice: 220,
-    alert: null,
-    openPopup: null
-};
-
-const actionMock = {
-    type: 'SOME_TYPE',
-    payload: {
-        product: { id: 1000003, price: 40, title: 'MASHIKO' },
-        quantity: 3
-    }
-};
 
 describe('Helper: ', () => {
     it('getFormattedPrice should transform number 40 to $40.00', () => {
@@ -80,8 +55,8 @@ describe('Reducer:', () => {
             alert: null,
             openPopup: null
         };
-        const actionMock = {type: 'SOME_NON_EXISTENT_TYPE'};
-        expect(cartReducer(initialStateMock, actionMock)).toEqual(initialStateMock);
+        const actionMock = { type: 'SOME_NON_EXISTENT_TYPE' };
+        expect(reducer.cartReducer(initialStateMock, actionMock)).toEqual(initialStateMock);
     });
 });
 
@@ -108,14 +83,14 @@ describe('Add product to the cart:', () => {
     it('if total products quantity in cart will exceed max limit - product is not added and alert is displayed', () => {
         state.totalQuantity = 19;
         action.payload.quantity = 2;
-        const updatedState = cartReducer(state, action);
+        const updatedState = reducer.cartReducer(state, action);
         expect(updatedState.totalQuantity).toBeLessThanOrEqual(constant.TOTAL_MAX_QUANTITY_IN_CART);
         expect(updatedState.alert).toBe(constant.TOTAL_MAX_QUANTITY_ALERT);
     });
 
     it('if product quantity in cart will exceed max limit per one product - product is not added and alert is displayed', () => {
         action.payload.quantity = 6;
-        const updatedState = cartReducer(state, action);
+        const updatedState = reducer.cartReducer(state, action);
         expect(updatedState.products['1000003']).not.toBeDefined();
         expect(updatedState.alert).toBe(constant.ONE_PRODUCT_MAX_QUANTITY_ALERT);
     });
@@ -150,7 +125,7 @@ describe('Remove product from the cart should lead to:', () => {
             }
         };
 
-        updatedState = cartReducer(state, action);
+        updatedState = reducer.cartReducer(state, action);
     });
 
     it('remove product id field from cart products object', () => {
@@ -195,7 +170,7 @@ describe('Decrease a product quantity in the cart should lead to:', () => {
             }
         };
 
-        updatedState = cartReducer(state, action);
+        updatedState = reducer.cartReducer(state, action);
     });
 
     it('decrease a product quantity by 1', () => {
@@ -216,7 +191,22 @@ describe('App component renders:', () => {
         const mockStore = configureStore();
         const store = mockStore({
             allProducts: [],
-            cart: stateMock
+            cart: {
+                products: {
+                    1000001: {
+                        product: { id: 1000001, price: 50, title: "Heme" },
+                        quantity: 2
+                    },
+                    1000003: {
+                        product: { id: 1000003, price: 40, title: 'MASHIKO' },
+                        quantity: 3
+                    }
+                },
+                totalQuantity: 5,
+                totalPrice: 220,
+                alert: null,
+                openPopup: null
+            }
         });
 
         const component = renderer.create(
@@ -265,41 +255,46 @@ describe('Fetch products:', () => {
     });
 });
 
-describe('Add product to cart:', () => {
-    const initialStateMock = {
-        products: {},
-        totalQuantity: 0,
-        totalPrice: 0,
-        alert: null,
-        openPopup: null
-    };
+describe.only('Add product to cart:', () => {
+    let store, state, product, action;
 
-    const mockProduct = { id: '1000001', price: 40, title: 'MASHIKO' };
+    beforeEach(() => {
+        const middlewares = [thunk];
+        const mockStore = configureStore(middlewares);
+        store = mockStore({});
 
-    const action = {
-        type: actionTypes.ADD_PRODUCT_TO_CART,
-        payload: {
-            product: mockProduct,
-            quantity: 1
-        }
-    };
+        state = {
+            products: {},
+            totalQuantity: 0,
+            totalPrice: 0,
+            alert: null,
+            openPopup: null
+        };
 
-    const middlewares = [thunk];
-    const mockStore = configureStore(middlewares);
+        product = { id: '1000001', price: 40, title: 'MASHIKO' };
 
+        action = {
+            type: actionTypes.ADD_PRODUCT_TO_CART,
+            payload: {
+                product: product,
+                quantity: 1
+            }
+        };
+    });
+    
     it('should return an action object with that product', async () => {
-        const store = mockStore({
-            allProducts: [],
-            cart: initialStateMock
-        });
-
-        await store.dispatch(actionCreators.addProductToCart(mockProduct, 1));
+        const spy = jest.spyOn(actionCreators, 'addProductToCart');
+        await store.dispatch(actionCreators.addProductToCart(product, 1));
         const actions = store.getActions();
-        console.log('actions', actions);
-        const state = store.getState();
-        console.log('state', state);
 
         expect(actions[0]).toEqual(action);
+        expect(spy).toHaveBeenCalled();
+    });
+    
+    it('call addProductToCart and setTimeout inside it', () => {
+        jest.useFakeTimers();
+        store.dispatch(actionCreators.addProductToCart(product, 1));
+        expect(setTimeout).toHaveBeenCalled();
+        expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), constant.ALERT_HIDE_DELAY);
     });
 });
-
