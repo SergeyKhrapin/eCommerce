132361e8a650bfe07ff1f146bebdb940c60409fb
SergeyKhrapin <khrapins@gmail.com>
2021-03-02 12:34:44 +0200

    Fixed props mutation

--- src/productQuantity/ProductQuantity.js
+++ src/productQuantity/ProductQuantity.js
@@ -4,18 +4,18 @@ import { INCREASE, DECREASE } from '../constants';
 import './ProductQuantity.scss';
 
 const ProductQuantity = props => {
-    let { handleQuantity, quantity, product } = props;
+    const { handleQuantity, quantity, product } = props;
 
     // From Product page - use quantity from props
     // From Cart page - use quantity from store
-    quantity = quantity ?? useSelector(store => store.cart.products[product.id].quantity);
+    const productQuantity = quantity ?? useSelector(store => store.cart.products[product.id].quantity);
 
     return (
         <div className="product-quantity">
             <div className="product-quantity__control">
                 <input
                     className="product-quantity__control--quantity"
-                    value={quantity}
+                    value={productQuantity}
                     type="text"
                     readOnly />
                 <div className="product-quantity__control--buttons" onClick={handleQuantity}>
