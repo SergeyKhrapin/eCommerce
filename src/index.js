0d1c094dee7349b551aaa2e2c637b9bd0b534cc6
SergeyKhrapin <khrapins@gmail.com>
2021-03-08 14:07:56 +0200

    Pre-renders - react snap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { render, hydrate } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import './index.css';
 import App from './App';
 
-render(
-    <Provider store={store}>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
-    </Provider>,
-    document.getElementById('root')
-);
+const rootElement = document.getElementById('root');
+const app = <Provider store={store}>
+                <BrowserRouter>
+                    <App />
+                </BrowserRouter>
+            </Provider>
+
+if (rootElement.hasChildNodes()) {
+    hydrate(app, rootElement);
+} else {
+    render(app, rootElement);
+}
