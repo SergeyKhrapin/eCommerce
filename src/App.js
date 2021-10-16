07a35a658bbd35c8666100faf0739a2ff72bf87a
SergeyKhrapin <khrapins@gmail.com>
2021-10-16 16:12:55 +0300

    transform class App component to functional one, remove direct import of store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Route, Link } from 'react-router-dom';
-import { store } from './redux/store';
 import { fetchProducts } from './redux/actionCreators';
 import logo from './logo.png';
 import Category from "./category/Category";
@@ -10,48 +9,40 @@ import Product from "./product/Product";
 import CartPopup from './cartPopup/CartPopup';
 import styles from "./app.module.css";
 
-class App extends Component {
-    constructor() {
-        super();
-    }
+const App = ({ openPopup, fetchProducts }) => {
+    useEffect(fetchProducts, [])
 
-    componentDidMount() {
-        store.dispatch(fetchProducts());
-    }
-
-    render() {
-        return (
-            <div className={`${styles.app} ${this.props.openPopup ? styles.appCartPopupOpened : ''}`} data-testid="app-container">
-                <header className={styles.header}>
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-2">
-                                <img src={logo} className={styles.logo} alt="logo" />
-                            </div>
-                            <div className="col-8">
-                                <nav className={styles.nav}>
-                                    <ul className={styles.list}>
-                                        <li><Link to="/">HOME</Link></li>
-                                        <li><a href="#">SHOP</a></li>
-                                        <li><a href="#">JOURNAL</a></li>
-                                        <li><a href="#">MORE</a></li>
-                                    </ul>
-                                </nav>
-                            </div>
-                            <div className="col-2">
-                                <CartPopup />
-                            </div>
+    return (
+        <div className={`${styles.app} ${openPopup ? styles.appCartPopupOpened : ''}`} data-testid="app-container">
+            <header className={styles.header}>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-2">
+                            <img src={logo} className={styles.logo} alt="logo" />
+                        </div>
+                        <div className="col-8">
+                            <nav className={styles.nav}>
+                                <ul className={styles.list}>
+                                    <li><Link to="/">HOME</Link></li>
+                                    <li><a href="#">SHOP</a></li>
+                                    <li><a href="#">JOURNAL</a></li>
+                                    <li><a href="#">MORE</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div className="col-2">
+                            <CartPopup />
                         </div>
                     </div>
-                </header>
+                </div>
+            </header>
 
-                <Route exact path="/" component={Category} />
-                <Route path="/cart" component={Cart} />
-                <Route path="/product/:id/:title" component={Product} />
-            </div>
-        );
-    }
-};
+            <Route exact path="/" component={Category} />
+            <Route path="/cart" component={Cart} />
+            <Route path="/product/:id/:title" component={Product} />
+        </div>
+    );
+}
 
 const mapStateToProps = state => {
     return {
@@ -59,4 +50,8 @@ const mapStateToProps = state => {
     };
 };
 
-export default connect(mapStateToProps)(App)
+const mapDispatchToProps = {
+    fetchProducts
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
